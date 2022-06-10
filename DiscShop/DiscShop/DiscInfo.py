c633350329ab21329df00cba75283845c7dc9f00
dpeacock <dpeacock@umich.edu>
2022-06-10 00:20:25 -0400

    progress on database

--- DiscShop/DiscShop/DiscInfo.py
+++ DiscShop/DiscShop/DiscInfo.py
@@ -7,9 +7,9 @@ class DiscGolfDatabase:
     def __init__(self):
         con = sl.connect('DiscGolf.db')
 
-    with con:
-        con.execute("""
-        CREATE TABLE ProPlayers (
+        with con:
+            con.execute("""
+            CREATE TABLE ProPlayers (
             team TEXT,
             rating INTEGER,
             name TEXT,
@@ -22,13 +22,77 @@ class DiscGolfDatabase:
             );
             """)
 
-    with con:
-        con.execute("""
+        with con:
+            con.execute("""
         CREATE TABLE Discs (
             name TEXT,
-            ...
-        );
+            speed INTEGER,
+            glide INTEGER,
+            turn INTEGER,
+            fade INTEGER,
+            type TEXT,
+            brand TEXT,
+            );
             """)
+
+        with con:
+            con.execute("""
+        CREATE TABLE Companies (
+            name TEXT,
+            location TEXT,
+            type TEXT,
+            yearFounded INTEGER,
+            );
+            """)
+
+     #does webscraping from https://alldiscs.com/ to autofill single entry of disc
+    def addOneDisc():
+         sql = 'INSERT INTO Discs (id, name, age) values(?, ?, ?)'
+         data = [
+                (1, 'Alice', 21),
+                (2, 'Bob', 22),
+                (3, 'Chris', 23)
+            ]
+         return
+
+        #adds all discs to Discs table
+    def addDiscs():
+
+        return
+
+
+        #does webscraping from ____________________________ to autofill single entry of pro player
+    def addOnePro():
+        sql = 'INSERT INTO Companies (id, name, age) values(?, ?, ?)'
+        data = [
+                (1, 'Alice', 21),
+                (2, 'Bob', 22),
+                (3, 'Chris', 23)
+            ]
+        return
+
+        #adds all pros to Pros table
+    def addPros():
+
+        return
+
+
+        #does webscraping from ____________________________ to autofill database of disc golf teams 
+    def addOneCompany():
+        sql = 'INSERT INTO ProPlayers (id, name, age) values(?, ?, ?)'
+        data = [
+                (1, 'Alice', 21),
+                (2, 'Bob', 22),
+                (3, 'Chris', 23)
+            ]
+        return
+
+        #adds all companies to Companies table
+    def addCompanies():
+
+        return
+
+
 
 
 
